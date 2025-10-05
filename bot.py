98fb3ba9bc9d935b28bc0f084de41767a8ce02b5
Qwesteqdji <ilyaklass12@gmail.com>
2025-10-05 16:20:51 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,12 @@ bot = telebot.TeleBot(token)
 def start(message):
     bot.reply_to(message, "Привет! Я бот для управления чатом.")
 
+
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+
+
 @bot.message_handler(commands=['ban'])
 def ban_user(message):
     if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
